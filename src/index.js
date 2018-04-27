25a6f43413a160dc87de9ecba948d9146bc4ac2a
da-matsumoto <da.mats1990@gmail.com>
2018-04-27 20:58:14 +0900

    npm install

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styles from './style.js'
+
+class ShowImage extends React.Component {
+    render () {
+        return (
+        <div>
+            <p>hello</p>
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <ShowImage />,document.getElementById('root')
+)
